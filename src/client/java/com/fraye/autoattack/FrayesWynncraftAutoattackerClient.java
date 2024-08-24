30458589fdadb68a76cdf3874dda27613773f928
kasperhhk@hotmail.com <kasperhhk@hotmail.com>
2024-08-24 21:00:55 +0200

    try fix commands

--- src/client/java/com/fraye/autoattack/FrayesWynncraftAutoattackerClient.java
+++ src/client/java/com/fraye/autoattack/FrayesWynncraftAutoattackerClient.java
@@ -1,33 +1,31 @@
 package com.fraye.autoattack;
 
-import static net.minecraft.server.command.CommandManager.*;
+import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.*;
 
 import com.mojang.brigadier.arguments.FloatArgumentType;
-
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.minecraft.text.Text;
 
 public class FrayesWynncraftAutoattackerClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
-			var main = literal("frayeauto");
-			var set = main.then(literal("set"))
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+			var set = literal("frayeauto").then(literal("set"))
 				.then(argument("value", FloatArgumentType.floatArg()))
 				.executes(context -> {
 					final float value = FloatArgumentType.getFloat(context, "value");
 					final int ticksPerAttack = value < 0.05 ? 1 : (int)((1f / value) * Settings.tickRate);
 					Settings.ticksPerAttack = ticksPerAttack;
-					context.getSource().sendFeedback(() -> Text.literal("Updated attacks per second to %s, ticks per attack %s".formatted(value, ticksPerAttack)), false);
+					context.getSource().sendFeedback(Text.literal("Updated attacks per second to %s, ticks per attack %s".formatted(value, ticksPerAttack)));
 					return 1;
 				});
-			var get = main.then(literal("get"))
+			var get = literal("frayeauto").then(literal("get"))
 				.executes(context -> {
 					final int ticksPerAttack = Settings.ticksPerAttack;
 					final float secondsPerAttack = (float)Settings.ticksPerAttack / (float)Settings.tickRate;
 					final float aps = 1 / secondsPerAttack;
-					context.getSource().sendFeedback(() -> Text.literal("Attacks per second is set to %s, ticks per attack %s".formatted(aps, ticksPerAttack)), false);
+					context.getSource().sendFeedback(Text.literal("Attacks per second is set to %s, ticks per attack %s".formatted(aps, ticksPerAttack)));
 					return 1;
 				});
 
--- src/client/java/com/fraye/autoattack/mixin/client/ExampleClientMixin.java
+++ src/client/java/com/fraye/autoattack/mixin/client/ExampleClientMixin.java
@@ -1,18 +1,9 @@
 package com.fraye.autoattack.mixin.client;
 
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
 import net.minecraft.util.Hand;
 
-import java.time.Duration;
-import java.time.Instant;
-import java.time.temporal.TemporalUnit;
-import java.util.Date;
-
-import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
