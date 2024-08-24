106f856626d7d409d7b46e1a61c05a98c1dd99d2
kasperhhk@hotmail.com <kasperhhk@hotmail.com>
2024-08-24 21:08:30 +0200

    ok i think commands work now

--- src/client/java/com/fraye/autoattack/FrayesWynncraftAutoattackerClient.java
+++ src/client/java/com/fraye/autoattack/FrayesWynncraftAutoattackerClient.java
@@ -11,26 +11,28 @@ public class FrayesWynncraftAutoattackerClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
-			var set = literal("frayeauto").then(literal("set"))
-				.then(argument("value", FloatArgumentType.floatArg()))
-				.executes(context -> {
-					final float value = FloatArgumentType.getFloat(context, "value");
-					final int ticksPerAttack = value < 0.05 ? 1 : (int)((1f / value) * Settings.tickRate);
-					Settings.ticksPerAttack = ticksPerAttack;
-					context.getSource().sendFeedback(Text.literal("Updated attacks per second to %s, ticks per attack %s".formatted(value, ticksPerAttack)));
-					return 1;
-				});
-			var get = literal("frayeauto").then(literal("get"))
-				.executes(context -> {
-					final int ticksPerAttack = Settings.ticksPerAttack;
-					final float secondsPerAttack = (float)Settings.ticksPerAttack / (float)Settings.tickRate;
-					final float aps = 1 / secondsPerAttack;
-					context.getSource().sendFeedback(Text.literal("Attacks per second is set to %s, ticks per attack %s".formatted(aps, ticksPerAttack)));
-					return 1;
-				});
-
-			dispatcher.register(set);
-			dispatcher.register(get);
+			dispatcher.register(literal("frayeauto")
+				.then(literal("set")
+					.then(argument("value", FloatArgumentType.floatArg())
+						.executes(context -> {
+							final float value = FloatArgumentType.getFloat(context, "value");
+							final int ticksPerAttack = value < 0.05 ? 1 : (int)((1f / value) * Settings.tickRate);
+							Settings.ticksPerAttack = ticksPerAttack;
+							context.getSource().sendFeedback(Text.literal("Updated attacks per second to %s, ticks per attack %s".formatted(value, ticksPerAttack)));
+							return 1;
+						})
+					)
+				)
+				.then(literal("get")
+					.executes(context -> {
+						final int ticksPerAttack = Settings.ticksPerAttack;
+						final float secondsPerAttack = (float)Settings.ticksPerAttack / (float)Settings.tickRate;
+						final float aps = 1 / secondsPerAttack;
+						context.getSource().sendFeedback(Text.literal("Attacks per second is set to %s, ticks per attack %s".formatted(aps, ticksPerAttack)));
+						return 0;
+					})
+				)
+			);
 		});
 	}
 }
