3e1896fb382a0c3e52c2556c76f951c0befe345b
kasperhhk@hotmail.com <kasperhhk@hotmail.com>
2024-08-24 19:25:27 +0200

    initial

---
+++ src/client/java/com/fraye/autoattack/FrayesWynncraftAutoattackerClient.java
@@ -0,0 +1,10 @@
+package com.fraye.autoattack;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FrayesWynncraftAutoattackerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
